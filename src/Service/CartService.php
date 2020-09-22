0ccb047ad4c5c24c71ae9e75c845830c9a212af6
Nanderson Castro <nandokstro@gmail.com>
2020-09-22 15:26:09 -0300

    Iniciando servico cart

---
+++ src/Service/CartService.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class CartService
+{
+	/**
+	 * @var SessionInterface
+	 */
+	private $session;
+
+	public function __construct(SessionInterface $session)
+	{
+
+		$this->session = $session;
+	}
+
+	public function getAll()
+	{
+		return $this->session->has('cart') ? $this->session->get('cart') : [];
+	}
+
+	public function addItem($item)
+	{
+		//Se tem carrinho na sessao
+		$cart = $this->getAll();
+
+		if(count($cart)) {
+			array_push($cart, $item);
+		} else {
+			$cart[] = $item;
+		}
+
+		$this->session->set('cart', $cart);
+
+		return true;
+	}
+
+	public function removeItem($item)
+	{
+
+	}
+}
