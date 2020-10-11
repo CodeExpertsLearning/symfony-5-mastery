32e2bb6b6b2c98b735069f4bd17a3a00664f3576
Nanderson Castro <nandokstro@gmail.com>
2020-10-11 16:36:21 -0300

    Carrinho e processo compra usuario

---
+++ src/Controller/CheckoutController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CheckoutController extends AbstractController
+{
+    /**
+     * @Route("/checkout", name="home_checkout", priority="10")
+     * @IsGranted("ROLE_USER")
+     */
+    public function index(Request $request)
+    {
+    	$session = $request->getSession();
+    	if(!$session->has('cart')) {
+    	    return $this->redirectToRoute('home');
+	    }
+
+    	$cart = $session->get('cart');
+
+        return $this->render( 'checkout.html.twig', compact('cart'));
+    }
+
+	/**
+	 * @Route("/checkout/thanks", name="home_checkout_finished", priority="10")
+	 * @IsGranted("ROLE_USER")
+	 */
+	public function thanks(Request $request)
+	{
+		if($request->getSession()->has('cart')) {
+			$request->getSession()->remove('cart');
+		}
+
+		return new Response('Obrigado por sua compra!');
+	}
+}
---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UserRegisterFormType;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="home_register", priority="10")
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $loginForm)
+    {
+    	$form = $this->createForm(UserRegisterFormType::class);
+		$form->handleRequest($request);
+
+		if($form->isSubmitted() && $form->isValid()) {
+
+			$user = $form->getData();
+
+			$password = $passwordEncoder->encodePassword($user, $form['password']->getData());
+			$user->setPassword($password);
+			$user->setRoles(['ROLE_USER']);
+
+			$em->persist($user);
+			$em->flush();
+
+			return $guardHandler->authenticateUserAndHandleSuccess($user, $request, $loginForm, 'main');
+		}
+
+        return $this->render( 'register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
